e3b114d9ec5fff86583323f54e31c6f3a5dbd06e
WuKeyu <wukeyu56@163.com>
2023-03-07 17:27:17 +0800

    补充用例

--- excuter.py
+++ excuter.py
@@ -1,10 +1,8 @@
 import os
 
-
 command1 = "pytest ./case/ --alluredir ./report/result"
 command2 = "allure generate ./report/result -o ./report/html --clean"
 
 # os.system('rmdir ./report/result')
 os.system(command1)
 os.system(command2)
-
